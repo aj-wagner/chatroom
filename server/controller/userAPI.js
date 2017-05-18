7d4d9b27f7829189b21e210ce10574a46faf2ef2
AJ Wagner <ajwagner@northwesternmutual.com>
2017-05-18 14:41:21 -0500

    write to file

--- server/controller/userAPI.js
+++ server/controller/userAPI.js
@@ -4,13 +4,12 @@ module.exports = (function(req, res){
 	return {
 		chatroom:function(req, res){
 			var name = {};
-			name[req.body.username] = 0;
-
-			var useObj = [];
-			useObj.push(name);
+			name[req.body.username] = 0;			
 
 			
 			if(!fs.existsSync('userObjects.json')){
+				var useObj = [];
+				useObj.push(name);
 
 				fs.writeFile('userObjects.json', JSON.stringify(useObj), function(err){console.log(err)});
 			}
@@ -19,24 +18,25 @@ module.exports = (function(req, res){
 
 				fs.readFile('userObjects.json', 'utf8', function(err, data){
 				var arrUserObjs = JSON.parse(data);
-//console.log(typeof arrUserObjs[0]);
 
 				for(var i=0; i<arrUserObjs.length; i++){
 					if(Object.keys(arrUserObjs[i]) == req.body.username){
-						res.render('chatroom', {username: req.body.username});
-				console.log(arrUserObjs[i]);
-
+						//don't update the file
+						res.render('chatroom');
 					}
 				}
-			
+				//user isn't found in the data file
+				arrUserObjs.push(name);
 
-				});
+				//push a new obj on the array from the file
+				fs.writeFile('userObjects.json', JSON.stringify(arrUserObjs), function(err){console.log(err)});
 
-			//	fs.writeFile('userObjects.json', JSON.stringify(useObj), function(err){console.log(err)});
-	
+				res.render('chatroom');
+			});
+
+
 			}
 			
-			res.render('chatroom');
 		}
 	}
 })()
